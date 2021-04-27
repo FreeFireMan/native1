94da3b36892a6ec8a598b3ff05d53e355d28b747
freefireman <beluh.c.s@gmail.com>
2021-04-27 21:03:34 +0300

    49:20

--- components/User.js
+++ components/User.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import {StyleSheet, Text, View} from "react-native";
+import {Button, Image, StyleSheet, Text, View} from "react-native";
 
 
 const User = ({item}) => {
-    return <View style={[styles.box, styles.item]}>
-        <Text>{item.id}--{item.name}</Text>
-    </View>
+    return (
+        <View style={[styles.box, styles.item]}>
+            <Image source={{
+                url: 'https://cdn.dribbble.com/users/56874/screenshots/1504502/rick-_rick-and-morty-cartoon_-pixel-art-by-dimaaaa-400_300_still.gif?compress=1&resize=400x300'
+            }}/>
+            <Text>{item.id}--{item.name}</Text>
+            <Button
+                title={'some button'}
+                onPress={() => alert('press')}
+            />
+        </View>
+    )
 }
 
 export default User;
